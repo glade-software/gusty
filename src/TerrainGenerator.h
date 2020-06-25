74dcf26e0192ca83bce0c483b78fb05b61250d35
Elias Reid <eliasreid@users.noreply.github.com>
2020-06-25 18:49:16 -0300

    removed comments

--- src/TerrainGenerator.h
+++ src/TerrainGenerator.h
@@ -26,43 +26,4 @@ namespace TerrainGen{
   std::optional<HeightMap> generateTerrain(TGenSeed seed, const Config& cfg);
 }
 
-// class TerrainGenerator{
-// public: 
-//   using HeightMap = std::vector<std::vector<float>>;
-  
-//   TerrainGenerator();
-//   std::optional<HeightMap> generateTerrain(const int seed = 0);
-
-//   void setHeight(const size_t height);
-//   void setWidth(const size_t width);
-//   void setGradientGridSize(const size_t grid_size);
-//   void setHeightScale(const float scale);
-//   void setHeightStepSize(const float step_size);
-
-//   void setHeightLimits(const float min, const float max);
-
-//   static std::string csvFromHeightMap(const HeightMap& height_map);
-
-// private:
-//   //Make class non-copyable
-//   TerrainGenerator(const TerrainGenerator&) = delete;
-//   TerrainGenerator& operator=(const TerrainGenerator&) = delete;
-
-//   static constexpr size_t PERMUTATION_TABLE_SIZE = 512;
-//   using PermutationArr = std::array<uint8_t, PERMUTATION_TABLE_SIZE>;
-
-//   PermutationArr calcPermutations(const int seed = 0);
-//   float grad(const uint8_t hash, const float x, const float y);
-//   float lerp(const float pt1, const float pt2, const float interpolate_amt);
-//   float fade(const float t);
-
-//   float discretize(const float val, const float floor, const float step_size) const;
-
-//   // Member variables for configuration
-//   //Give them default values, use setters for configuring
-
-
-
-// };
-
 #endif
